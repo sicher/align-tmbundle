364d00f71424a645dfc2b57afdd81e76a0f5f133
Mikael Säker <mikael@sicher.org>
2009-04-04 23:03:31 +0200

    Initial commit.

---
+++ README.markdown
@@ -0,0 +1,121 @@
+# Align.tmbundle
+
+This bundle contains a couple of simple tools for aligning text. There are already some aligning commands in other bundles, but this one is created to supply a more generic alignment functionality. The bundle is modeled to some extent after the align plugin available for the vim editor.
+
+To use it, just press CTRL-SHIFT-A, and then select the appropriate action:
+
+* Align (brings up a input dialog where custom alignment patterns can be entered)
+* Align assignments.
+* Align comments.
+* Align text columns.
+
+## Examples:
+
+### Align assignments.
+
+	Before:
+	
+	a=1;
+	ab=2;
+	abc=3;
+	abcd=4;
+	abcde=5;
+	a+=1;
+	ab+=2;
+	abc+=3;
+	this_is_func();
+	abcd+=4;
+	abcde+=5;
+	a/=1;
+	ab/=2;
+	abc/=3;
+	abcd/=4;
+	abcde/=5;
+
+	After:
+
+	a      = 1;
+	ab     = 2;
+	abc    = 3;
+	abcd   = 4;
+	abcde  = 5;
+	a     += 1;
+	ab    += 2;
+	abc   += 3;
+	this_is_func();
+	abcd  += 4;
+	abcde += 5;
+	a     /= 1;
+	ab    /= 2;
+	abc   /= 3;
+	abcd  /= 4;
+	abcde /= 5;
+
+### Align comments. 
+
+	Before:
+
+	int     a;              /* a   */
+	float   b;              /* b   */
+	double *c=NULL;              /* b   */
+	char x[5]; /* x[5] */
+	struct  abc_str abc;    /* abc */
+	struct  abc_str *pabc;    /* pabc */
+	static   int     a;              /* a   */
+	static   float   b;              /* b   */
+	static   double *c=NULL;              /* b   */
+	static   char x[5]; /* x[5] */
+		static   struct  abc_str abc;    /* abc */
+	static   struct  abc_str *pabc;    /* pabc */
+	
+	After:
+
+	int     a;                      /* a    */
+	float   b;                      /* b    */
+	double *c=NULL;                 /* b    */
+	char x[5];                      /* x[5] */
+	struct  abc_str abc;            /* abc  */
+	struct  abc_str *pabc;          /* pabc */
+	static   int     a;             /* a    */
+	static   float   b;             /* b    */
+	static   double *c=NULL;        /* b    */
+	static   char x[5];             /* x[5] */
+	static   struct  abc_str abc;   /* abc  */
+	static   struct  abc_str *pabc; /* pabc */
+
+### Align text columns
+
+	Before:
+	
+	Name Age Married Hairstyle
+	Jake 23 yes wild
+	Jane 32 no long
+	Jimmy 44 no bald
+	Eddie 22 yes slick
+	Edna 31 no greasy
+	Lorna 34 yes curly
+	Artie 41 no crewcut
+	
+	After:
+
+	Name  Age Married Hairstyle
+	Jake  23  yes     wild
+	Jane  32  no      long
+	Jimmy 44  no      bald
+	Eddie 22  yes     slick
+	Edna  31  no      greasy
+	Lorna 34  yes     curly
+	Artie 41  no      crewcut
+
+## Installation
+
+Run this:
+ 
+	$ cd ~/Library/Application\ Support/TextMate/Bundles
+	$ git clone git://github.com/sicher/align-tmbundle.git Align.tmbundle
+	$ osascript -e 'tell app "TextMate" to reload bundles'
+
+You can also use GetBundles:
+
+	$ cd ~/Library/Application\ Support/TextMate/Bundles
+	$ svn co http://svn.textmate.org/trunk/Review/Bundles/GetBundles.tmbundle/
---
+++ Support/lib/align.rb
@@ -0,0 +1,257 @@
+#!/usr/bin/env ruby
+#
+#  tm_align.rb
+#  
+#  By Mikael Säker. All rights reserved.
+# 
+
+# TODO: Align word columns is bugged...
+
+require ENV['TM_SUPPORT_PATH'] + '/lib/textmate'
+require ENV['TM_SUPPORT_PATH'] + '/lib/ui'
+
+module Align
+  
+  def expand_tabs(str)
+    return str.gsub(/\t/, " " * ENV["TM_TAB_SIZE"].to_i)
+  end
+
+  def empty?(str)
+    if str.strip.length == 0 then
+      return true
+    end
+    return false
+  end
+
+  def find_block_start(lines, currentline)
+    # If current line is empty...
+    if empty?(lines[currentline]) then
+      return currentline
+    end
+    # Figure out the indent on the current line
+    block_indent = expand_tabs(lines[currentline]).index(/\S/).to_i
+    startline = currentline
+    # Look backwards until beginning of this block...
+    (currentline - 1).downto(0) do |i|
+      line_indent = expand_tabs(lines[i]).index(/\S/).to_i
+      if line_indent != block_indent || empty?(lines[i]) then
+        return i + 1
+      end
+      startline -= 1
+    end
+    return startline
+  end
+
+  def find_block_end(lines, currentline)
+    # If current line is empty...
+    if empty?(lines[currentline]) then
+      return currentline
+    end
+    # Figure out the indent on the current line
+    block_indent = expand_tabs(lines[currentline]).index(/\S/).to_i
+    # Look backwards until beginning of this block...
+    endline = currentline
+    (currentline + 1).upto(lines.length - 1) do |i|
+      line_indent = expand_tabs(lines[i]).index(/\S/).to_i
+      if line_indent != block_indent || empty?(lines[i]) then
+        return i - 1
+      end
+      endline += 1
+    end
+    return endline
+  end
+
+  # Return an array containing one array for each line in the range which in turn containst
+  # the widths between each ocurrence of the pattern matches. Whitespace is not counted.
+  def rangefieldwidths(range, patterns, includemathinglines)
+    p = patternregexp(patterns)
+
+    rangefieldwidths = Array.new
+    range.each do |l|
+      if !includemathinglines and !p.match(l) then
+        next
+      end  
+      linefieldwidths = Array.new
+      l.split(p).each do |s|
+        linefieldwidths.push(s.strip.length)
+      end
+      rangefieldwidths.push(linefieldwidths)
+    end
+    return rangefieldwidths
+  end
+
+  def patternregexp(pattern)
+    ep = Regexp.escape(pattern)
+    np = ep.gsub(/\\ /, '|')
+    np = np.gsub(/\\\\/, '\\')
+    return Regexp.new(np)
+  end
+
+  def numcolumns(rangefieldwidths)
+    max = 0
+    rangefieldwidths.each do |r|
+      if r.length > max
+        max = r.length
+      end
+    end
+    return max
+  end
+
+  def widestfieldincolumn(rangefieldwidths, column)
+    max = 0
+    rangefieldwidths.each do |r|
+      if !r[column].nil? then
+        if r[column] > max then
+          max = r[column]
+        end      
+      end
+    end
+    return max
+  end
+
+  def targetfieldwidths(rangefieldwidths)
+    tfw = Array.new 
+    0.upto(numcolumns(rangefieldwidths) - 1) do |c|
+      tfw.push(widestfieldincolumn(rangefieldwidths, c))
+    end
+    return tfw
+  end
+
+  def widestseparator(range, p)
+    max = 0;
+    range.each do |l|
+      i = 0
+      l.split(p).each do |s|
+        i += s.length
+        sep = l.slice(i, l.length).slice(p)
+        if sep then
+          if sep.length > max then
+            max = sep.length
+          end 
+          i += sep.length
+        end
+      end
+    end
+    return max
+  end
+
+  def reformatrange(range, patterns, options)
+    rangefieldwidths = rangefieldwidths(range, patterns, options[:include_non_matching_lines])  
+    tfw = targetfieldwidths(rangefieldwidths)
+    p = patternregexp(patterns)
+    if options[:right_justify_separators] then
+      wsep = widestseparator(range, p)
+    end
+    if @pre_selection then
+      puts @pre_selection
+    end
+    range.each do |l|
+      if !options[:include_non_matching_lines] and !p.match(l) then
+        puts l
+        next
+      end
+      c = 0
+      i = 0
+      indent = expand_tabs(l).index(/\S/).to_i
+      $stdout.write " " * indent
+      l.split(p).each do |s|
+        i += s.length
+        sep = l.slice(i, l.length).slice(p)
+
+        # Deal with the field and field right hand whitespace
+        field = ""
+        fieldws = ""
+        if s.strip.length > 0 then
+          if options[:right_field_whitespace] then
+            fieldws = " "
+          end
+          if options[:right_justify_fields] then
+            field = "#{s.strip.rjust(tfw[c])}"
+          elsif !sep then
+            # no ws before newline
+            field = "#{s.strip}"
+            fieldws = ""
+          else
+            field = "#{s.strip.ljust(tfw[c])}"
+          end
+        end
+
+        separator = ""
+        separatorws = ""
+        if sep then
+          if options[:right_justify_separators] then
+            separator = "#{sep.rjust(wsep)}"
+          else
+            separator = sep
+          end        
+          if options[:right_separator_whitespace] then
+            separatorws = " "
+          end
+          i += sep.length
+          
+          # no ws before newline
+          if l.slice(i, l.length).strip == "" then
+            separatorws = ""
+          end
+        end
+        
+        # Print it!
+        $stdout.write "#{field}#{fieldws}#{separator}#{separatorws}"
+        c += 1
+      end
+      # Newline
+      puts
+    end
+    if @post_selection then
+      puts @post_selection
+    end
+  end
+
+  def get_selection
+    lines = STDIN.readlines()
+    selection = Array.new
+    selection_done = ENV.member?("TM_SELECTED_TEXT")
+
+    # Depending if user has no selection, we need to create the selection
+    if !selection_done then
+      @pre_selection = Array.new
+      @post_selection = Array.new    
+      current_lineno = ENV["TM_LINE_NUMBER"].to_i - 1
+      # Fix trailing newline which causes a "nil" last line
+      if lines[current_lineno].nil? then
+        lines[current_lineno] = ""
+      end
+      blockstart = find_block_start(lines, current_lineno)
+      blockend = find_block_end(lines, current_lineno)
+      
+      # Store pre- and post selection
+      0.upto(blockstart - 1) do |i|
+        @pre_selection.push(lines[i])
+      end 
+      (blockend + 1).upto(lines.length - 1) do |i|
+        @post_selection.push(lines[i])
+      end 
+      
+      blockstart.upto(blockend) do |i|
+        selection.push(lines[i])
+      end
+    else
+      @pre_selection  = nil
+      @post_selection = nil
+      selection       = lines
+    end
+    return selection
+  end
+
+  def request_patterns(default)
+    options           = Hash.new
+    options[:title]   = "Align Selection"
+    options[:prompt]  = "Enter alignment anchor pattern(s):"
+    options[:default] = default
+    return TextMate::UI.request_string(options)  
+  end
+  
+  extend self
+end
+
+############################################################################
